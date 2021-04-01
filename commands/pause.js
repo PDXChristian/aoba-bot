2ca8af8e5fecba4278908eecaac0880f0bdf3e57
PDXWegman <8883699+PDXChristian@users.noreply.github.com>
2021-04-01 05:03:20 -0700

    Finished moving the pause command to DiscordJS v12

--- commands/pause.js
+++ commands/pause.js
@@ -1,8 +1,8 @@
 module.exports = function pause(message, serverQueue) {
-  if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to pause the music!')
+  if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to pause the music!')
   if(serverQueue.connection.dispatcher.paused) return message.channel.send('The music is already paused')
   message.channel.send('Music has been paused')
-  serverQueue.connection.dispatcher.pause()
+  serverQueue.connection.dispatcher.pause(true)
 }
 
 module.exports.help = {
