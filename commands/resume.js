f04bae6da4c27f62dab728277f063196b5b5cb3b
PDXWegman <8883699+PDXChristian@users.noreply.github.com>
2021-04-01 05:04:13 -0700

    Finished moving the resume command to DiscordJS v12

--- commands/resume.js
+++ commands/resume.js
@@ -1,5 +1,5 @@
 module.exports = function resume(message, serverQueue) {
-  if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to pause the music!')
+  if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to pause the music!')
   if(!serverQueue.connection.dispatcher.paused) return message.channel.send('The music is already playing')
   message.channel.send('Music has been resumed')
   serverQueue.connection.dispatcher.resume()
