9beb8fef5bf954403ead8a78ea495edd39a52f75
PDXWegman <8883699+PDXChristian@users.noreply.github.com>
2021-04-01 05:10:00 -0700

    Finished moving the stop command to DiscordJS v12

--- commands/stop.js
+++ commands/stop.js
@@ -1,5 +1,5 @@
 module.exports = function stop(message, serverQueue) {
-  if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!')
+  if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!')
   if (serverQueue === undefined) return message.channel.send('Nothing is playing')
   serverQueue.songs = []
   serverQueue.connection.dispatcher.end()
