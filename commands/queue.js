7c72c51f3c010e4b61a404e770ab1b76da651af1
PDXWegman <8883699+PDXChristian@users.noreply.github.com>
2021-04-01 05:03:42 -0700

    Finished moving the queue command to DiscordJS v12

--- commands/queue.js
+++ commands/queue.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js')
 
 module.exports = function songQueue(message, serverQueue) {
-  if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to list the songs!')
+  if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to list the songs!')
   if (!serverQueue) return message.channel.send('There is nothing in the queue!')
-  const embed = new Discord.RichEmbed()
+  const embed = new Discord.MessageEmbed()
   .setTitle('<:yt_icon_rgb:582082533332615168> Now Playing')
   .setColor(0xFF0000)
   serverQueue.songs.forEach( song => {
