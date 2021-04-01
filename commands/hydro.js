4a11caf8260b75cba997d96f130bd9b469ac0486
PDXWegman <8883699+PDXChristian@users.noreply.github.com>
2021-04-01 05:01:31 -0700

    Finished moving the hydro command to DiscordJS v12

--- commands/hydro.js
+++ commands/hydro.js
@@ -4,13 +4,13 @@ const Discord = require('discord.js')
 module.exports = async message => {
   
   const randPost = await RedditSimple.RandomPost('HydroHomies')
-  const embed = new Discord.RichEmbed() 
+  const embed = new Discord.MessageEmbed()
     .setTitle(randPost[0].data.title)
     .setImage(randPost[0].data.url)
     .setColor(0x0079FF)
     .setURL('https://reddit.com' + randPost[0].data.permalink)    
-  message.channel.send(embed)   
-  console.log(randPost[0].data.url)
+  message.channel.send(embed)
+
 }
 
 module.exports.help = {
